133d9af79cdef3e4a3fa228cc69e82132e51f46b
muro3r <wac6et@gmail.com>
2020-07-13 00:49:36 +0900

    feat: Add basic feature
    Create item
    Update item status

--- todos/urls.py
+++ todos/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index)]
+app_name = "todos"
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("cerate", views.create, name="create"),
+    path("update", views.update, name="update"),
+]
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,45 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Todo
 
 
 def index(request):
-    return HttpResponse("index")
+    return HttpResponse("todos:index")
+
+
+class IndexView(ListView):
+    template_name = "index.html"
+    context_object_name = "todos"
+
+    def get_queryset(self):
+        return Todo.objects.all()
+
+
+def create(request):
+    todo = Todo(title=request.POST["title"])
+    todo.save()
+    return HttpResponseRedirect(reverse("todos:index"))
+
+
+def update(request):
+    process = []
+
+    try:
+        update_request = request.POST.getlist("todo")
+
+        for todo in Todo.objects.all():
+            todo.status = bool(str(todo.id) in update_request)
+
+            process.append(todo)
+
+    except (KeyError, Todo.DoesNotExist):
+        return render(request, "index.html", {"error_message": "!!Error!!"})
+    else:
+        # 処理成功時に更新する
+        for todo in process:
+            todo.save()
+
+        return HttpResponseRedirect(reverse("todos:index"))
